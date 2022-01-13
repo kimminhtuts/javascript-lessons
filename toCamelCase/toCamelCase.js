f8f154afcfce3b1798d7d9daa696825ff5697789
Kim Minh <kimminh.tuts@gmail.com>
2022-01-13 22:44:30 +0700

    Update

--- toCamelCase/toCamelCase.js
+++ toCamelCase/toCamelCase.js
@@ -1,8 +1,10 @@
-const string1 = "To Camel Case"
-const string2 = "TO CAMEL CASE"
-const string3 = "to camel case"
-const string4 = "tO CAMel CaSE"
-const abc = " iehieje www "
+const input = "   To  CAmEl CASE    " // => toCamelCase
 
-const result = abc.toLowerCase().split(" ").map((e, idx) => idx ? `${e[0]?.toUpperCase()}${e.slice(1)}`: e).join("")
+const result = input
+  .toLowerCase()
+  .trim()
+  .split(" ")
+  .map((e, index) => e ? index ? `${e[0]?.toUpperCase()}${e.slice(1)}` : e : "")
+  .join("")
+
 console.log(result)
